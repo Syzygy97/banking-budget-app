0d91fafab8847abaeea3aa6016f2aae1767aa3ee
Syzygy97 <aimielmarvin26@gmail.com>
2022-09-27 22:18:06 +0800

    fixed title page folder location

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 import SignUpPage from "./pages/Sign-Up-Page/SignUpPage";
 
 import NavBar from "./components/NavBar/navBar";
-import TitlePage from "./components/TitlePage/TitlePage";
-import SignIn from "./components/Sign-In/signIn"
+import TitlePage from "./pages/TitlePage/TitlePage";
+import SignIn from "./components/Sign-In/signIn";
 import "./App.css";
 
 function App() {
