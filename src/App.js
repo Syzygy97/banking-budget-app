a6f39244aadf98a67bd2b7c0ab8950a4a82bbc86
Jessarey <jessareyapuli@gmial.com>
2022-09-27 20:11:16 +0800

    Fixes merge conflict

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import SignIn from "./components/Sign-In/SignIn";
 import SignUpPage from "./pages/Sign-Up-Page/SignUpPage";
 
+import NavBar from "./components/NavBar/NavBar";
+import TitlePage from "./components/TitlePage/TitlePage";
+import SignIn from "./components/Sign-In/SignIn";
+import "./App.css";
+
 function App() {
   return (
-    <>
-      {/* <SignUpPage /> */}
-      <SignIn />
-    </>
+    <div>
+      <NavBar></NavBar>
+      <div className="main-page">
+        <TitlePage></TitlePage>
+        <SignIn></SignIn>
+      </div>
+    </div>
   );
 }
 
